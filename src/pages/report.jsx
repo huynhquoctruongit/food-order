02cec85d526e09ee0aadcc05b302bf7fff4e1d8d
Truonghq <huynhquoctruongit@gmail.com>
2024-08-13 17:39:38 +0700

    update sort answer

--- src/pages/report.jsx
+++ src/pages/report.jsx
@@ -14,6 +14,7 @@ import AxiosAPI from "@/libs/api/axios-client.ts"
 import { useState } from "react"
 import isoWeek from 'dayjs/plugin/isoWeek';
 import { useToast } from "@/components/ui/use-toast"
+import { Button } from "@/components/ui/button-hero.jsx";
 
 const Report = () => {
     const { toast } = useToast()
@@ -23,6 +24,7 @@ const Report = () => {
     const [dataReport, setDataReport] = useState()
     const [currentSelect, setCurrentSelect] = useState()
     const [userSelect, setSelectUser] = useState({})
+    const [valueChange, setChange] = useState("")
     const dateCurrent = currentSelect?.[0] + "T05:00:00.000Z"
     const { data: orderToday, mutate: mutateOrder } = useSWR(currentSelect?.[0] &&
         `/items/order_84?fields=*,user.*&filter[date_created][_between]=${currentSelect?.[0]},${currentSelect?.[4]}T24:00:00.000Z`
@@ -44,7 +46,12 @@ const Report = () => {
     const admin = userSelect?.fullname == "Hồng Phạm"
 
     const onUpdateOrder = (e, item, ortherList, date, type) => {
-        const price = e.target.value
+        var priceInput = e.target.value
+
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            priceInput = eval(priceInput)
+        }
+        const price = priceInput
         const params = {
             order_id: ortherList?.id || ortherList?.orderId,
             note: "Nước",
@@ -138,12 +145,11 @@ const Report = () => {
     useEffect(() => {
         setCurrentSelect(weekList[weekUrl])
     }, [weekUrl])
-
     return (
-        <div className="py-[23px] bg-white text-black min-h-[calc(100vh-64px)]">
+        <div className="bg-[url(/background.png)] bg-contain py-[23px] bg-white text-gray-600 min-h-[calc(100vh-64px)]">
             <div className="px-[20px] md:px-[100px]">
                 <div className="flex justify-start">
-                    <select defaultValue={weekUrl} onChange={(e) => selectWeek(e)} className="p-[10px] bg-white text-black border-[1px] border-black">
+                    <select defaultValue={weekUrl} onChange={(e) => selectWeek(e)} className="rounded-md p-[10px] bg-white text-gray-600 border-[1px] border-pastel-pink">
                         <option disabled selected>Chọn tuần</option>
                         {Object.keys(weekList).map(function (key, index) {
                             const title = weekList[key][0]?.slice(-2) + "-" + weekList[key][4]?.slice(-2)
@@ -155,49 +161,54 @@ const Report = () => {
                     </select>
                 </div>
                 <div className="flex justify-between items-center my-[20px]">
-                    <h1 className="font-bold mb-[20px]">Báo cáo</h1>
+                    <h1 className="text-[20px] md:text-3xl font-bold text-gray-600 text-center">Báo cáo</h1>
                     <div>
-                        {admin && <div className="border-[1px] border-black px-[30px] py-[10px] cursor-pointer bg-green-600 text-white" onClick={onSave}>Lưu</div>}
+                        {admin && <Button variant="default" size="default" onClick={onSave}>
+                            Lưu lại
+                        </Button>}
                     </div>
                 </div>
                 <Table>
-                    <TableHeader className="border-l-[1px] border-l-black border-r-[1px] border-r-black">
-                        <TableRow className="border-t-[1px] border-t-black border-b-black">
-                            <TableHead className="w-[200px] px-0 font-bold border-r-[1px] border-r-black text-center">Tên</TableHead>
+                    <TableHeader className="border-l-[1px] border-l-pastel-pink border-r-[1px] border-r-pastel-pink">
+                        <TableRow className="rounded-md border-t-[1px] border-t-pastel-pink border-b-pastel-pink">
+                            <TableHead className="bg-white rounded-md w-[200px] px-0 font-bold border-r-[1px] border-r-pastel-pink text-center">Tên</TableHead>
                             {currentSelect?.map((elm, index) => {
                                 return (
-                                    <TableHead className="font-bold px-0 items-center mx-auto border-r-[1px] border-r-black">
+                                    <TableHead className="bg-white font-bold px-0 items-center mx-auto border-r-[1px] border-r-pastel-pink">
                                         <p className="text-center p-[10px]">Thứ {index + 2} ({dayjs(elm).format('DD/MM')})</p>
-                                        <div className="flex items-center border-t-[1px] border-t-black p-[10px]"><p className="w-[50%] text-center">Đồ ăn</p><> | </> <p className="w-[50%] text-center whitespace-nowrap">Đồ uống</p></div>
+                                        <div className="flex items-center border-t-[1px] border-t-pastel-pink p-[10px]"><p className="w-[50%] text-center">Đồ ăn</p><> | </> <p className="w-[50%] text-center whitespace-nowrap">Đồ uống</p></div>
                                     </TableHead>
                                 )
                             })}
-                            <TableHead className="font-bold px-0 items-center mx-auto border-r-[1px] border-r-black">
+                            <TableHead className="bg-white rounded-md font-bold px-0 items-center mx-auto border-r-[1px] border-r-pastel-pink">
                                 <p className="text-center p-[10px]">Đã chuyển</p>
                             </TableHead>
-                            <TableHead className="text-right font-bold">Amount</TableHead>
+                            <TableHead className="bg-white text-right font-bold">Amount</TableHead>
                         </TableRow>
                     </TableHeader>
-                    <TableBody className="border-l-[1px] border-l-black border-r-[1px] border-r-black">
+                    <TableBody className="border-l-[1px] border-l-pastel-pink border-r-[1px] border-r-pastel-pink">
                         {groupedData?.map((userItem, index) => {
                             const recipt = reciptList?.find((elm) => elm?.user?.fullname === userItem?.user?.fullname && dayjs(elm.date_start).format("YYYY-MM-DD") == currentSelect?.[0])
                             return (
-                                <TableRow className={`${index %2 == 0 ? "bg-slate-200" : "bg-white"} hover:bg-unset`} key={userItem.user.fullname}>
+                                <TableRow className={`${index % 2 == 0 ? "bg-pastel-pink/30" : "bg-white"} hover:bg-unset`} key={userItem.user.id + index + "group"}>
                                     <TableCell className="font-medium text-left p-2"><div className="p-[6px]">{userItem.user.fullname}</div></TableCell>
                                     {currentSelect?.map((elm, index) => {
                                         const ortherList = userItem?.items?.find((ortherItem) => ortherItem.name === "orther-food" && dayjs(ortherItem.date_created).format("YYYY-MM-DD") == elm)
                                         const riceList = userItem?.items?.filter((riceItem) => riceItem.name !== "orther-food" && dayjs(riceItem.date_created).format("YYYY-MM-DD") == elm)
                                         const match = ortherList && dayjs(ortherList.date_created).format("YYYY-MM-DD") == elm
-                                        console.log(riceList,'riceList');
+                                        const date = dayjs(elm + "T12:00:00+07:00").format("YYYY-MM-DD")
+                                        const valueInput = userItem.user.id + "-" + date
+                                        const valueWater = dataReport?.[valueInput]?.price
+                                        console.log(valueWater, 'valueWater');
                                         return (
-                                            <TableCell key={weekUrl + index + "-elm-wrapper"} className="text-left p-2">
-                                                <input key={weekUrl + index + "-elm-input1"} disabled className="p-[6px] w-[50%] text-center bg-transparent text-black select-none" value={riceList?.length ? riceList?.length * 35 : ""}></input>
-                                                <input key={weekUrl + index + "-elm-input2"} disabled={!admin} className={`p-[6px] w-[50%] text-center bg-transparent text-black ${admin && "border-[1px] border-black"}`} defaultValue={match ? ortherList?.price : ""} onChange={(e) => onUpdateOrder(e, userItem, ortherList, dayjs(elm + "T12:00:00+07:00").format("YYYY-MM-DD"), "orther-food")}></input>
+                                            <TableCell key={userItem.user.id + date + index + "-elm-wrapper"} className="text-left p-2">
+                                                <input key={userItem.user.id + date + index + "-elm-input1"} disabled className="rounded-md p-[6px] w-[50%] text-center bg-transparent text-gray-600 select-none" value={riceList?.length ? riceList?.length * 35 : ""}></input>
+                                                <input key={userItem.user.id + date + index + "-elm-input2"} disabled={!admin} className={`rounded-md p-[6px] w-[50%] text-center bg-transparent text-gray-600 ${admin && "border-[1px] border-pastel-pink"}`} value={valueWater} defaultValue={match ? ortherList?.price : ""}  onKeyUp={(e) => onUpdateOrder(e, userItem, ortherList, date, "orther-food")}></input>
                                             </TableCell>
                                         )
                                     })}
                                     <TableCell className="text-left p-2">
-                                        <input disabled={!admin} className={`p-[6px] w-[100%] text-center bg-transparent text-black ${admin && "border-[1px] border-black"}`} defaultValue={recipt?.amount && Math.ceil(recipt?.amount)} onInput={(e) => onUpdateOrder(e, userItem, recipt, currentSelect[0], "recipt")}></input>
+                                        <input disabled={!admin} className={`rounded-md p-[6px] w-[100%] text-center bg-transparent text-gray-600 ${admin && "border-[1px] border-pastel-pink"}`} defaultValue={recipt?.amount && Math.ceil(recipt?.amount)} onKeyUp={(e) => onUpdateOrder(e, userItem, recipt, currentSelect[0], "recipt")}></input>
                                     </TableCell>
                                     <TableCell className="text-right font-bold p-2">{getMount(userItem)}k</TableCell>
                                 </TableRow>
@@ -205,7 +216,7 @@ const Report = () => {
                         })}
                     </TableBody>
                     <TableFooter>
-                        <TableRow className="bg-black hover:bg-black hover:text-white text-white">
+                        <TableRow className="bg-pastel-pink hover:bg-pastel-pink hover:text-white text-white">
                             <TableCell className="font-bold text-left" colSpan={5}>Tổng</TableCell>
                             <TableCell className="text-right font-bold" colSpan={5}>Tự tính dùm cái i</TableCell>
                         </TableRow>
