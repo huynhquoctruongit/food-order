cc0ed1452dc1b0ac162ce2870ffd9c20010850f1
nhatnn <newbie.dev.js@gmail.com>
2024-08-31 18:18:58 +0700

    update

--- src/modules/report/component/chart-price.jsx
+++ src/modules/report/component/chart-price.jsx
@@ -6,7 +6,12 @@ const ChartPrice = () => {
   const { profile } = useAuth();
   const isMd = useMediaQuery("(min-width: 768px)");
   const payload = {
-    filter: { company: { _eq: profile.company }, status: { _eq: "published" }, user_created: { _eq: profile.id } },
+    filter: {
+      company: { _eq: profile.company },
+      status: { _eq: "published" },
+      user_created: { _eq: profile.id },
+      date_created: { _gte: "$NOW(-5 week)" },
+    },
     groupBy: ["week(date_created)"],
     aggregate: { sum: ["price", "delivery_fee"] },
   };
@@ -14,50 +19,56 @@ const ChartPrice = () => {
   const gap = 50;
   const height = isMd ? 48 : 32;
   const week = [...(report?.data || [])];
-
-  if (report?.data.length)
-    week.unshift(
-      { sum: { price: 0, delivery_fee: 0 }, date_created_week: null },
-      { sum: { price: 0, delivery_fee: 0 }, date_created_week: null },
-    );
+  const again = Array.from({ length: 5 - week.length }, (_, i) => ({
+    sum: { price: 0, delivery_fee: 0 },
+    date_created_week: null,
+  }));
 
   const listPrice = Array.from({ length: 7 }, (_, i) => gap * i).reverse();
 
   return (
     <div className="text-left">
-      <div className="rounded-md w-full p-3 md:p-6 flex border border-primary-01/40 ring-[6px] bg-white ring-primary-01/5 ring-offset-0">
-        <div>
-          <div className="">
-            {listPrice.map((price, index) => (
-              <div key={price + "price" + index} className="h-10 flex items-end border-dashed border-b">
-                {price}
-              </div>
-            ))}
-          </div>
-          <div className="flex items-center ">
-            <div className="w-12 md:w-20 h-10 relative"></div>
-            <div className="items-center gap-4 md:gap-10 flex">
-              {week.map((item, index) => {
-                const value = parseInt(item.sum.price) + (parseInt(item.sum.delivery_fee) || 0);
-                const label = item.date_created_week === null ? null : `T.${item.date_created_week}`;
-                return (
-                  <div height={{ height: height }} className=" relative w-10 cursor-pointer" key={label + "prices" + index}>
-                    <div className="font-bold text-gray-400 pt-2 text-center text-sm">{label}</div>
-                    <div
-                      className="absolute bottom-full left-0 w-10 hover:bg-primary-01 bg-pastel-pink duration-200 rounded-t-sm"
-                      style={{ height: Math.ceil((value * height) / gap) }}
-                    >
-                      {value > 0 && (
-                        <div className="absolute bottom-full left-0 w-10 text-xs text-center rounded-t-sm mb-2">{value}</div>
-                      )}
+      {week.length > 0 && (
+        <div className="rounded-md w-full p-3 md:p-6 flex border border-primary-01/40 ring-[6px] bg-white ring-primary-01/5 ring-offset-0">
+          <div>
+            <div className="">
+              {listPrice.map((price, index) => (
+                <div key={price + "price" + index} className="h-10 flex items-end border-dashed border-b">
+                  {price}
+                </div>
+              ))}
+            </div>
+            <div className="flex items-center ">
+              <div className="w-12 md:w-20 relative"></div>
+              <div className="items-center gap-4 md:gap-10 flex">
+                {[...again, ...week].map((item, index) => {
+                  const value = parseInt(item.sum.price) + (parseInt(item.sum.delivery_fee) || 0);
+                  const label = item.date_created_week === null ? null : `T.${item.date_created_week}`;
+                  return (
+                    <div height={{ height: height }} className=" relative w-10 cursor-pointer" key={label + "prices" + index}>
+                      <div className="font-bold text-gray-400 pt-2 text-center text-sm">{label}</div>
+                      <div
+                        className="absolute bottom-full left-0 w-10 hover:bg-primary-01 bg-pastel-pink duration-200 rounded-t-sm"
+                        style={{ height: Math.ceil((value * height) / gap) }}
+                      >
+                        {value > 0 && (
+                          <div className="absolute bottom-full left-0 w-10 text-xs text-center rounded-t-sm mb-2">{value}</div>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
+
+      {week.length === 0 && (
+        <div className="text-center text-gray-400 border border-primary-01/40 ring-[6px] bg-white ring-primary-01/5 ring-offset-0 min-h-[300px] md:min-w-[350px]">
+          Không có dữ liệu
+        </div>
+      )}
     </div>
   );
 };
--- src/modules/report/component/info.jsx
+++ src/modules/report/component/info.jsx
@@ -4,29 +4,34 @@ import useSWR from "swr";
 
 const LabelInfo = () => {
   const { profile } = useAuth();
+  const payloadHistory = {
+    filter: { company: { _eq: profile.company }, status: { _eq: "published" }, user_created: { _eq: profile.id } },
+    limit: 5,
+  };
   const payload = {
     filter: { company: { _eq: profile.company }, status: { _eq: "published" }, user_created: { _eq: profile.id } },
     aggregate: { sum: ["price", "delivery_fee"], count: ["id"] },
   };
   const { data: report } = useSWR(["/items/order", payload]);
-  const { data: historyRes } = useSWR(["/items/order?limit=5"]);
+  const { data: historyRes } = useSWR(["/items/order", payloadHistory]);
   const list = report?.data || [];
-  const total = list.reduce((total, item) => total + parseInt(item.sum.price) + (parseInt(item.sum.delivery_fee) || 0), 0);
-  const totalOlder = list.reduce((total, item) => total + parseInt(item.count.id), 0);
+  const total = list.reduce((total, item) => total + parseInt(item.sum.price || 0) + (parseInt(item.sum.delivery_fee) || 0), 0);
+  const totalOlder = list.reduce((total, item) => total + parseInt(item.count.id || 0), 0);
   const history = historyRes?.data || [];
+  const again = Array.from({ length: 5 - history.length }, (_, i) => ({ id: i, name: "" }));
 
   return (
     <div className="w-full flex-1 relative text-left">
       <div className="flex flex-col gap-6 md:absolute top-0 left-0 w-full h-full">
         <div className="gap-6 grid md:grid-cols-2">
           <div className="border-primary-01/40 border rounded-lg h-fit ring-[6px] bg-white ring-primary-01/5 ring-offset-0">
-            <div className="p-6">
+            <div className="p-5">
               <div className="text-lg text-black font-black uppercase">Tổng tiền</div>
               <div className="text-4xl text-primary-01 mt-4 font-bold ">{total} cá</div>
             </div>
           </div>
           <div className="border-primary-01/40 border rounded-lg h-fit ring-[6px] bg-white ring-primary-01/5 ring-offset-0">
-            <div className="p-6">
+            <div className="p-5">
               <div className="text-lg text-black font-black uppercase">Tổng món</div>
               <div className="text-4xl text-primary-01 mt-4 font-bold">{totalOlder} Món</div>
             </div>
@@ -34,11 +39,11 @@ const LabelInfo = () => {
         </div>
         <div className="relative w-full flex-1">
           <div className="border-primary-01/40 border rounded-lg col-span-2 text-left ring-[6px] bg-white ring-primary-01/5 ring-offset-0">
-            <div className="p-6">
+            <div className="p-5">
               <div className="text-lg text-black font-black">LỊCH SỬ 5 MÓN</div>
-              <div className="flex flex-col gap-0.5 mt-1 text-left">
-                {history.map((item) => (
-                  <div key={item.id}> - {item.name}</div>
+              <div className="flex flex-col gap-1 mt-1 text-left">
+                {[...history, ...again].map((item) => (
+                  <div key={item.id}> - {item.name || "..........."}</div>
                 ))}
               </div>
             </div>
