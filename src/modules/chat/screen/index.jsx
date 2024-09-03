512c79ba2429408da8e3fcf6de1435b6ea3b396c
nhatnn <newbie.dev.js@gmail.com>
2024-09-03 22:46:06 +0700

    update

--- src/modules/chat/screen/index.jsx
+++ src/modules/chat/screen/index.jsx
@@ -123,7 +123,7 @@ const ChatWiget = () => {
         <div className="fixed cursor-pointer bottom-4 flex flex-col gap-2 md:bottom-10 right-4 md:right-10 z-100">
           <Link to="/relax" className=" rounded-full p-2 bg-white shadow-md relative group " onClick={() => setShow(true)}>
             <img src="/health.png" className="w-8 h-8 object-contain" />
-            <div className="absolute hidden group-hover:flex bottom-full right-full w-96 h-96 bg-white rounded-full items-center justify-center">
+            <div className="absolute hidden group-hover:flex bottom-full border border-dashed border-primary-01/50 right-full w-96 h-96 bg-white rounded-full items-center justify-center">
               <img src="/health.png" className="w-80 h-80 object-contain " />
             </div>
             <div className="w-2 h-2 rounded-full absolute top-0 right-0 animate-ping bg-primary-01"></div>
