a15c4fbc010394f0be18ff97effb96d4e4e2e100
nhatnn <newbie.dev.js@gmail.com>
2024-09-10 10:42:21 +0700

    update

--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,10 +1,6 @@
 import { useAuth } from "@/hooks/use-auth";
-import ReportByAdmin from "./report-by-admin";
 import ReportByUser from "../../modules/report/screen/user";
 const Report = () => {
-  const { profile, isLoading } = useAuth();
-  const isAdmin = profile?.permission_to_update_order;
-  if (isLoading) return null;
-  return isAdmin ? <ReportByAdmin /> : <ReportByUser />;
+  return <ReportByUser />;
 };
 export default Report;
