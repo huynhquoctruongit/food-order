21e692a6d4b91dcb19f76f1e584c3f86fc6daeb0
nhatnn <newbie.dev.js@gmail.com>
2024-09-18 20:41:53 +0700

    update ui

--- src/modules/order/components/cursor.jsx
+++ src/modules/order/components/cursor.jsx
@@ -2,8 +2,6 @@ import { createImage } from "@/lib/helper";
 import { MousePointer2 } from "lucide-react";
 
 export default function Cursor({ color, x, y, message, profile }) {
-  console.log(profile);
-
   return (
     <div
       className="pointer-events-none absolute top-0 left-0 z-[100000]"
@@ -14,7 +12,11 @@ export default function Cursor({ color, x, y, message, profile }) {
       <MousePointer2 className={" stroke-none fill-primary-01"} />
       <div className="absolute top-full left-1/2 flex items-center gap-2">
         <div className="w-12 h-12 bg-white shadow-md rounded-full relative">
-          <img src={createImage(profile.avatar, 100)} alt="avatar" className="w-full h-full object-cover rounded-full animate-rotate" />
+          <img
+            src={createImage(profile.avatar, 100)}
+            alt="avatar"
+            className="w-full h-full object-cover rounded-full animate-rotate"
+          />
           <div className="absolute top-full left-0 whitespace-nowrap mt-2 bg-white shadow-xl rounded-md px-2 py-0.5">
             {profile.name}
           </div>
