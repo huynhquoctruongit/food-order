8afe54f760adb5723b59ab87cb3e19789d9fe84c
nhatnn <newbie.dev.js@gmail.com>
2024-09-26 21:00:36 +0700

    update home page

--- src/modules/home/component/interactive.jsx
+++ src/modules/home/component/interactive.jsx
@@ -1,6 +1,9 @@
-import { FollowerPointer, FollowPointer } from "@/components/ui/follow-pointer";
+import { FollowerPointer } from "@/components/ui/follow-pointer";
 import { AnimateLineText } from "@/components/widget/animate-text";
 import { motion } from "framer-motion";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import { users } from "../helper/user-data";
+import { createImage } from "@/lib/helper";
 
 const TitleComponent = ({ author }) => (
   <div className="flex space-x-2 items-center">
@@ -9,7 +12,7 @@ const TitleComponent = ({ author }) => (
       height="32"
       width="32"
       alt="thumbnail"
-      className="rounded-full w-8 h-8 min-w-5 border-2 bg-white border-white"
+      className="rounded-full w-6 h-6 min-w-6 border-1 bg-white border-white"
     />
     <p>{author.name}</p>
   </div>
@@ -33,6 +36,11 @@ const Nhat = {
   avatar: "/foods/mon-2.png",
 };
 
+const Anonymus = {
+  name: "Vy Nguyễn",
+  avatar: "/foods/mon-2.png",
+};
+
 const dataTransfer = [
   { title: "OvenBaked Germaô tô kê", image: "/foods/mon-1.png" },
   { title: "OvenBaked Germa OvenBaked q Germa", image: "/foods/mon-2.png" },
@@ -43,15 +51,14 @@ const dataTransfer = [
 const Interactive = () => {
   return (
     <>
-      <AnimateLineText className="alata mb-10 text-5xl justify-center mt-4 text-primary-01 uppercase text-center">
+      <AnimateLineText className="alata mt-20 mb-10 text-5xl justify-center text-primary-01 uppercase text-center">
         Tương tác cùng với nhau
       </AnimateLineText>
-      <FollowerPointer title={<TitleComponent author={blogContent} />} className="">
-        <div className="h-fit bg-gradient-to-tr from-pastel-pink/5 to-primary-01/30 flex items-center justify-center min-h-[60vh] w-screen relative">
-          <div className="absolute w-96 h-72 z-10 top-10/12 right-10 bg-[#fca98ddf] blur-3xl"></div>
-          <div className="absolute w-96 h-72 z-10 top-2/12 -right-10 bg-[#edfffd9e] blur-3xl"></div>
-
-          <div className="root-wrapper mx-auto relative flex gap-10 z-10">
+      <FollowerPointer title={<TitleComponent author={blogContent} />} className="relative">
+        <div className="h-fit bg-gradient-to-tr from-pastel-pink/5 to-primary-01/30 py-32 w-screen relative">
+          <div className="absolute w-96 h-72 z-10 top-5/12 right-32 bg-[#fca98ddf] blur-3xl"></div>
+          <div className="absolute w-96 h-72 z-10 top-2/12 right-10 bg-[#edfffd9e] blur-3xl"></div>
+          <div className="root-wrapper mx-auto relative flex gap-10 z-10 px-32">
             <div className="grid grid-cols-2 h-fit items-center flex-wrap w-2/3 gap-5">
               {dataTransfer.map((item) => (
                 <div
@@ -73,8 +80,11 @@ const Interactive = () => {
               <Notification />
             </div>
           </div>
-          <ElementCursor x={400} y={200} vx={7} vy={4} title={<TitleComponent author={Linh} />} />
-          <ElementCursor delay={0.5} x={700} y={100} vx={3} vy={6} title={<TitleComponent author={Nhat} />} />
+
+          <ElementCursor x={400} y={270} vx={7} vy={4} title={<TitleComponent author={Linh} />} />
+          <ElementCursor delay={0.5} x={800} y={120} vx={3} vy={6} title={<TitleComponent author={Nhat} />} />
+          <ElementCursor delay={0.3} x={500} y={400} vx={3} vy={6} title={<TitleComponent author={Anonymus} />} />
+          <FloatingDockDemo />
         </div>
       </FollowerPointer>
     </>
@@ -85,12 +95,12 @@ export default Interactive;
 
 const Notification = () => {
   return (
-    <div className="w-60 px-2 bg-white rounded-md border border-dashed border-pastel-pink flex items-center">
+    <div className="w-60 px-2 bg-white rounded-md border border-dashed border-pastel-pink flex items-center hover:shadow-button duration-200">
       <div className="w-10 h-10 min-w-10 p-1 border-pastel-pink border-2 rounded-full">
         <img className="w-full h-full object-contain" src={"/foods/mon-1.png"} alt="" />
       </div>
       <div className="p-2">
-        <div className="text-gray-500 text-left text-sm">Vừa đặt món</div>
+        <div className="text-gray-500 text-left text-xs">Vừa đặt món</div>
         <div className="text-gray-800 text-left line-clamp-1 text-xs">Cá chiên mắm đù tuyệt vời</div>
       </div>
     </div>
@@ -173,3 +183,36 @@ const ElementCursor = ({ delay = 0, x, y, title, vx, vy }) => {
     </motion.div>
   );
 };
+
+export function FloatingDockDemo() {
+  const links = users.slice(0, 7).map((item) => {
+    return {
+      title: item.first_name + " " + item.last_name,
+      icon: (
+        <img
+          src={createImage(item.avatar, 200)}
+          className="h-full w-full object-cover  rounded-full text-neutral-500 dark:text-neutral-300"
+        />
+      ),
+      href: "#",
+    };
+  });
+
+  const more = {
+    title: "Thành viên còn lại",
+    icon: (
+      <div className="h-full w-full object-cover text-xs bg-white rounded-full text-primary-01 flex items-center justify-center font-bold ">
+        +20
+      </div>
+    ),
+    href: "#",
+  };
+  return (
+    <div className="flex items-center justify-center w-full">
+      <FloatingDock
+        mobileClassName="" // only for demo, remove for production
+        items={[...links, more]}
+      />
+    </div>
+  );
+}
